fc4f625d19f5a163e1f07670bee2110550a50681
GRatierODU <grati001@odu.edu>
2026-09-13 12:34:44 -0300

    Add extra-bundled catalog loader.

--- lib/data/extra-bundled.ts
+++ lib/data/extra-bundled.ts
@@ -18,6 +18,23 @@ import { E14 } from "./extra-chunks/e14";
 import { E15 } from "./extra-chunks/e15";
 import { E16 } from "./extra-chunks/e16";
 import { E17 } from "./extra-chunks/e17";
+import { E18 } from "./extra-chunks/e18";
+import { E19 } from "./extra-chunks/e19";
+import { E20 } from "./extra-chunks/e20";
+import { E21 } from "./extra-chunks/e21";
+import { E22 } from "./extra-chunks/e22";
+import { E23 } from "./extra-chunks/e23";
+import { E24 } from "./extra-chunks/e24";
+import { E25 } from "./extra-chunks/e25";
+import { E26 } from "./extra-chunks/e26";
+import { E27 } from "./extra-chunks/e27";
+import { E28 } from "./extra-chunks/e28";
+import { E29 } from "./extra-chunks/e29";
+import { E30 } from "./extra-chunks/e30";
+import { E31 } from "./extra-chunks/e31";
+import { E32 } from "./extra-chunks/e32";
+import { E33 } from "./extra-chunks/e33";
+import { E34 } from "./extra-chunks/e34";
 
 export const EXTRA_CHUNKS = [
   E00,
@@ -38,6 +55,23 @@ export const EXTRA_CHUNKS = [
   E15,
   E16,
   E17,
+  E18,
+  E19,
+  E20,
+  E21,
+  E22,
+  E23,
+  E24,
+  E25,
+  E26,
+  E27,
+  E28,
+  E29,
+  E30,
+  E31,
+  E32,
+  E33,
+  E34,
 ];
 
 export async function loadExtra(): Promise<CompactPrompt[]> {
